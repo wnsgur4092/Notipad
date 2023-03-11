5139af18138b78c6d2db186ecaa14721e8a01747
wnsgur4092 <wnsgur4092@gmail.com>
2023-03-11 16:49:33 +1100

    Initial Commit

---
+++ Notipad/NotipadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotipadApp.swift
+//  Notipad
+//
+//  Created by JunHyuk Lim on 11/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotipadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
